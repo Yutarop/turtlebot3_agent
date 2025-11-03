9f7fbcc8cf4168c69c3f07bff91a3d98631a4946
Yutarop <katoyutaro1122@icloud.com>
2025-11-03 04:34:58 +0000

    change tools for real-world-use

--- turtlebot3_agent/tools/all_tools.py
+++ turtlebot3_agent/tools/all_tools.py
@@ -1,14 +1,9 @@
-# from turtlebot3_agent.tools.api_tools import get_information_from_internet
 from turtlebot3_agent.tools.motion_tools import (
-    make_navigate_to_pose_tool,
+    make_move_linear_tool,
+    make_rotate_tool,
     make_transform_odom_to_map_tool,
 )
-
-# from turtlebot3_agent.tools.navigation_tools import make_navigate_to_goal_tool
-from turtlebot3_agent.tools.sensor_tools import (
-    make_get_lidar_scan_tool,
-    make_start_camera_display_tool,
-)
+from turtlebot3_agent.tools.sensor_tools import make_get_lidar_scan_tool
 from turtlebot3_agent.tools.status_tools import make_get_turtle_pose_tool
 
 
@@ -23,9 +18,9 @@ def make_all_tools(node) -> list:
         list: List of tools available to the agent
     """
     return [
+        make_move_linear_tool(node),
+        make_rotate_tool(node),
         make_transform_odom_to_map_tool(node),
-        make_start_camera_display_tool(node),
-        make_navigate_to_pose_tool(node),
         make_get_turtle_pose_tool(node),
         make_get_lidar_scan_tool(node),
     ]
