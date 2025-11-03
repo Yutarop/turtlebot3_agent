201f81de85141070587e02ca3ac36356d67127bb
Yutarop <katoyutaro1122@icloud.com>
2025-11-03 06:49:35 +0000

    remove unnecessary tools

--- turtlebot3_agent/tools/all_tools.py
+++ turtlebot3_agent/tools/all_tools.py
@@ -1,9 +1,4 @@
-from turtlebot3_agent.tools.motion_tools import (
-    make_move_linear_tool,
-    make_rotate_tool,
-    make_transform_odom_to_map_tool,
-)
-from turtlebot3_agent.tools.sensor_tools import make_get_lidar_scan_tool
+from turtlebot3_agent.tools.motion_tools import make_move_linear_tool, make_rotate_tool
 from turtlebot3_agent.tools.status_tools import make_get_turtle_pose_tool
 
 
@@ -20,7 +15,5 @@ def make_all_tools(node) -> list:
     return [
         make_move_linear_tool(node),
         make_rotate_tool(node),
-        make_transform_odom_to_map_tool(node),
         make_get_turtle_pose_tool(node),
-        make_get_lidar_scan_tool(node),
     ]
